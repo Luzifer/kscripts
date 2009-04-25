7af5248c268155f0500ca37d6bdeac1a063d8e30
Luzifer <knut@ahlers.me>
2009-04-25 04:37:57 -0700

    Addeed license header

--- feed2twitter.rb
+++ feed2twitter.rb
@@ -1,5 +1,23 @@
 #!/usr/bin/ruby
 
+####
+# feed2twitter v.0.1 (c) 2009 by Knut Ahlers
+# WWW: http://blog.knut.me - Mail: knut@ahlers.me
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+####
+
 # Use this to configure the application
 config = {
   :feed => "http://url/to/my/feed/",              # URL of the feed
